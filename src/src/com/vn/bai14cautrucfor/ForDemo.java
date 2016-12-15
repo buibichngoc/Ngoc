b3b1f4173d3ed25a9997c4519d2f4e72574cdd24
buibichngoc <buibichngocat8c@gmail.com>
2016-12-15 10:15:16 +0700

    ngoc

---
+++ src/src/com/vn/bai14cautrucfor/ForDemo.java
@@ -0,0 +1,12 @@
+package com.vn.bai14cautrucfor;
+
+/**
+ * Created by banhu on 12/15/2016.
+ */
+public class ForDemo {
+    public static void main(String[] args) {
+        for (int i = 100; i < 110; i++) {
+            System.out.println("i: " + i);
+        }
+    }
+}
