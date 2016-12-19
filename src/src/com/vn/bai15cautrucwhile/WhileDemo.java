f0773f05b861cf3494c0a24d1f4d3993a6f09552
buibichngoc <buibichngocat8c@gmail.com>
2016-12-19 09:08:16 +0700

    ngoc

--- src/src/com/vn/bai15cautrucwhile/WhileDemo.java
+++ src/src/com/vn/bai15cautrucwhile/WhileDemo.java
@@ -2,6 +2,8 @@ package com.vn.bai15cautrucwhile;
 
 /**
  * Created by banhu on 12/15/2016.
+ * While thuong dung trong doc file, Doc bao gio het thi dung
+ * Cau truc For thi khong the dung duoc vi khong co gia tri co dinh
  */
 public class WhileDemo {
     public static void main(String[] args) {
@@ -14,8 +16,9 @@ public class WhileDemo {
         }
 /**        while(a > -10){
             System.out.println("a: " + a);
-            //a++; *//** lap vo han**//*
-        }*/
+            //a++; lap vo han
+        }
+**/
         while (b < 10){
             System.out.println("b: " + b);
             b += 3;
---
+++ src/src/com/vn/bai17array/ArrayDemo.java
@@ -0,0 +1,37 @@
+package com.vn.bai17array;
+
+/**
+ * Created by banhu on 12/16/2016.
+ * Mang la 1 loat DL thanh phan khac nhau sap sep thanh 1 day...
+ */
+public class ArrayDemo {
+
+    public static void main(String[] args) {
+        int[] arrInt1 = new int[10]; //mang 1chieu co 10 ptu nhung chu biet cac giatri
+        int[] arrInt2 = {2, 3, 4, 5}; //mang 2 chieu co 4 ptu va ptu co gitri ro rang
+
+        int[][] arrInt3 = new int[5][4];
+
+        String[] arrString = new String[5];
+        ArrayDemo[] arrDemos = new ArrayDemo[5];
+
+        System.out.println("arrInt1:");
+        for (int i = 0; i < arrInt1.length; i++) {
+            System.out.println(arrInt1[i]);
+        }
+
+        System.out.println("arrInt2:");
+        for (int i = 0; i < arrInt2.length; i++) {
+            System.out.println(arrInt2[i]);
+        }
+
+        System.out.println("arrInt3:");
+        for (int j = 0; j < 5; j++) {
+            for (int k = 0; k < 4; k++) {
+                System.out.print(arrInt3[j][k] + " ");
+            }
+            System.out.println("");
+        }
+    }
+
+}
