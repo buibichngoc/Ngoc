b7378977ec195d118368074af17988d21a6fe972
buibichngoc <buibichngocat8c@gmail.com>
2016-12-19 10:03:14 +0700

    ngoc

--- src/src/com/vn/bai19classabstractclassinterface/Bai19.java
+++ src/src/com/vn/bai19classabstractclassinterface/Bai19.java
@@ -5,31 +5,21 @@ package com.vn.bai19classabstractclassinterface;
  */
 public class Bai19 {
     //Ten class phai trung voi public class
-
 }
 
 class Ngoc {
     //Mot class gom thuoc tinh va phuong thuc
     private String name;//thuoc tinh
-
     public void study() { //phuong thuc
-
     }
-
 }
 
 interface BanhU {
-    /**
-     * aaa
-     */
     public final static String COUNTRY_NAME = "Viet Nam";
-
     public void show();
-
 }
 
 abstract class Beo {
     private  String name;
-
     public abstract void move();
 }
