c74ae4aafe7558ed8f8ca1a2c5695715d2961c38
buibichngoc <buibichngocat8c@gmail.com>
2016-12-20 13:32:54 +0700

    ngoc

--- src/src/com/javabasic/vn/bai28/cautructrycatch/TryCatchDemo.java
+++ src/src/com/javabasic/vn/bai28/cautructrycatch/TryCatchDemo.java
@@ -10,14 +10,27 @@ package com.javabasic.vn.bai28.cautructrycatch;
 public class TryCatchDemo {
     public static void main(String[] args) {
         String test = "BuiBichNgoc";
+        //1 try co the co nhieu catch
         try {
             System.out.println("Before: ");
-            System.out.println(test.substring(50));
+            //System.out.println(test.substring(50)); //doc cac ptu thu 50
+            System.out.println(test.substring(6));
+            int a = 5;
+            int b = 0;
+            System.out.println("a/b= " + (a / b));
         } catch (StringIndexOutOfBoundsException e) {
             System.out.println("Error: " + e.toString());
         }
+        catch(ArithmeticException e){
+            System.out.println("error: " + e.toString());
+        }
+        //try catch co them thang finally la tphan cuoi
+        //finally du loi hay ko loi exception hay ko exception no van thuc hien
+        //
+        finally {
+            System.out.println("Always done");
+        }
         System.out.println("After: ");
-
 
     }
 }
